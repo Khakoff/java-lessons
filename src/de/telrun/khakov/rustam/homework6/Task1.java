0438aa4a5c44bcb9377d9b2eb01beea255b593b3
Khakoff <35781622+Khakoff@users.noreply.github.com>
2022-10-11 20:16:31 +0300

    add HW 6 Task 1 (#2)
    * add HW 6 Task 1
    
    * HW 6: fix remarks

---
+++ src/de/telrun/khakov/rustam/homework6/Task1.java
@@ -0,0 +1,12 @@
+package de.telrun.khakov.rustam.homework6;
+
+/**
+ * @author Rustam Khakov
+ */
+public class Task1 {
+	public static void main(String[] args) {
+		int divider = 1;
+		int division = 3;
+		System.out.println(divider % division == 0);
+	}
+}
