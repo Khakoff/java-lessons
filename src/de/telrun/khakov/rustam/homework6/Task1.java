3894e2fbb887547ef3b88c9ae87f4635ff309f40
Rustam <hakov.rustam@mail.ru>
2022-10-11 20:13:26 +0300

    add HW 6 Task 1

---
+++ src/de/telrun/khakov/rustam/homework6/Task1.java
@@ -0,0 +1,12 @@
+package de.telrun.khakov.rustam.homework6;
+
+/**
+ * @author Rustam Khakov
+ */
+public class Task1 {
+	public static void main(String[] args) {
+		int test = 1;
+		int test1 = 3;
+		System.out.println(test % test1 == 0);
+	}
+}
